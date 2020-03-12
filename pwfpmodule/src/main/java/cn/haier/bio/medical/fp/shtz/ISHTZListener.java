432e41c8f54b49f975a93eb764e92bb27920b113
peiwen.liu <peiwen.liu@picovr.com>
2020-03-12 10:04:30 +0800

    Make project

---
+++ pwfpmodule/src/main/java/cn/haier/bio/medical/fp/shtz/ISHTZListener.java
@@ -0,0 +1,27 @@
+package cn.haier.bio.medical.fp.shtz;
+
+public interface ISHTZListener {
+    void onSHTZReady();
+    void onSHTZConnected();
+    void onSHTZException();
+
+    void onSHTZRegistStated();
+    void onSHTZRegistTimeout();
+    void onSHTZRegistFailured();
+    void onSHTZFingerAlreadyExists();
+    void onSHTZRegistStepChanged(int step);
+    void onSHTZRegistSuccessed(int finger);
+
+    void onSHTZUploadStated();
+    void onSHTZUploadFailured();
+    void onSHTZUploadSuccessed();
+
+    void onSHTZNoFingerExist();
+    void onSHTZDownloadStated();
+    void onSHTZDownloadFailured();
+    void onSHTZDownloadSuccessed();
+    void onSHTZFingerUNRegistered();
+    void onSHTZFingerRecognized(int finger);
+
+    boolean isFingerValid(int finger);
+}
---
+++ pwfpmodule/src/main/java/cn/haier/bio/medical/fp/shtz/SHTZManager.java
@@ -0,0 +1,81 @@
+package cn.haier.bio.medical.fp.shtz;
+
+import java.util.List;
+
+import cn.qd.peiwen.pwtools.EmptyUtils;
+
+/***
+ * 上海图正指纹模块
+ *
+ */
+public class SHTZManager {
+    private SHTZSerialPort serialPort;
+    private static SHTZManager manager;
+
+    public static SHTZManager getInstance() {
+        if (manager == null) {
+            synchronized (SHTZManager.class) {
+                if (manager == null)
+                    manager = new SHTZManager();
+            }
+        }
+        return manager;
+    }
+
+    private SHTZManager() {
+
+    }
+
+    public void init(ISHTZListener listener) {
+        if(EmptyUtils.isEmpty(this.serialPort)){
+            this.serialPort = new SHTZSerialPort();
+            this.serialPort.init(listener);
+        }
+    }
+
+    public void enable() {
+        if(EmptyUtils.isNotEmpty(this.serialPort)){
+            this.serialPort.enable();
+        }
+    }
+
+    public void disable() {
+        if(EmptyUtils.isNotEmpty(this.serialPort)){
+            this.serialPort.disable();
+        }
+    }
+
+    public void regist() {
+        if(EmptyUtils.isNotEmpty(this.serialPort)){
+            this.serialPort.regist();
+        }
+    }
+
+    public void uplaod(List<String> fileList) {
+        if(EmptyUtils.isNotEmpty(this.serialPort)){
+            this.serialPort.uplaod(fileList);
+        }
+    }
+
+    public void download(String filePath) {
+        if(EmptyUtils.isNotEmpty(this.serialPort)){
+            this.serialPort.download(filePath);
+        }
+    }
+
+    public boolean isBusy() {
+        if(EmptyUtils.isNotEmpty(this.serialPort)){
+            this.serialPort.isBusy();
+        }
+        return false;
+    }
+
+
+    public void release() {
+        if(EmptyUtils.isNotEmpty(this.serialPort)){
+            this.serialPort.release();
+            this.serialPort = null;
+        }
+    }
+}
+
